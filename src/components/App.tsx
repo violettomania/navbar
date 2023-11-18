1ea8545de01ca9da5fcdaf98e1265a966c61ccba
violettomania <danielpolya@protonmail.com>
2023-11-18 20:05:48 +0100

    refactor using refs

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,44 +1,62 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { FaBars } from 'react-icons/fa';
 import { links, social } from '../data';
+import logo from '../assets/logo.svg';
 
-export default function App() {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+const App = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef<HTMLDivElement>(null);
+  const linksRef = useRef<HTMLUListElement>(null);
+
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
+  useEffect(() => {
+    if (linksRef.current && linksContainerRef.current) {
+      const linksHeight = linksRef.current.getBoundingClientRect().height;
+      if (showLinks) {
+        linksContainerRef.current.style.height = `${linksHeight}px`;
+      } else {
+        linksContainerRef.current.style.height = '0px';
+      }
+    }
+  }, [showLinks]);
 
   return (
-    <main>
-      <nav>
-        <div className='nav-center'>
-          <div className='nav-header'>
-            <img src='/assets/logo.svg' className='logo' alt='logo' />
-            <button
-              className='nav-toggle'
-              onClick={() => setIsMenuOpen(!isMenuOpen)}
-            >
-              <FaBars />
-            </button>
-          </div>
-          <div
-            className='links-container'
-            style={{ height: isMenuOpen ? '160px' : '0px' }}
-          >
-            <ul className='links'>
-              {links.map(({ id, url, text }) => (
+    <nav>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} className='logo' alt='logo' />
+          <button className='nav-toggle' onClick={toggleLinks}>
+            <FaBars />
+          </button>
+        </div>
+        <div className='links-container' ref={linksContainerRef}>
+          <ul className='links' ref={linksRef}>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
                 <li key={id}>
                   <a href={url}>{text}</a>
                 </li>
-              ))}
-            </ul>
-          </div>
-          <ul className='social-icons'>
-            {social.map(({ id, url, icon }) => (
+              );
+            })}
+          </ul>
+        </div>
+        <ul className='social-icons'>
+          {social.map((socialIcon) => {
+            const { id, url, icon } = socialIcon;
+            return (
               <li key={id}>
                 <a href={url}>{icon}</a>
               </li>
-            ))}
-          </ul>
-        </div>
-      </nav>
-    </main>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
   );
-}
+};
+
+export default App;

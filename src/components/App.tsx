0ba0641255f31f30a92d1b5b2c58824e862f5d91
violettomania <danielpolya@protonmail.com>
2023-11-18 19:18:54 +0100

    feature: toggle menu on and off in responsive mode

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,27 @@
+import React from 'react';
 import { FaBars } from 'react-icons/fa';
 import { links, social } from '../data';
 
 export default function App() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
   return (
     <main>
       <nav>
         <div className='nav-center'>
           <div className='nav-header'>
             <img src='/assets/logo.svg' className='logo' alt='logo' />
-            <button className='nav-toggle'>
+            <button
+              className='nav-toggle'
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+            >
               <FaBars />
             </button>
           </div>
-          <div className='links-container' style={{ height: '0px' }}>
+          <div
+            className='links-container'
+            style={{ height: isMenuOpen ? '160px' : '0px' }}
+          >
             <ul className='links'>
               {links.map(({ id, url, text }) => (
                 <li key={id}>

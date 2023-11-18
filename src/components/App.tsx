ec403982de702c92dcea585f9ed15972c28a4510
violettomania <danielpolya@protonmail.com>
2023-11-18 19:02:47 +0100

    use FA icon for hamburger menu

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,4 @@
+import { FaBars } from 'react-icons/fa';
 import { links, social } from '../data';
 
 export default function App() {
@@ -8,17 +9,7 @@ export default function App() {
           <div className='nav-header'>
             <img src='/assets/logo.svg' className='logo' alt='logo' />
             <button className='nav-toggle'>
-              <svg
-                stroke='currentColor'
-                fill='currentColor'
-                strokeWidth='0'
-                viewBox='0 0 448 512'
-                height='1em'
-                width='1em'
-                xmlns='http://www.w3.org/2000/svg'
-              >
-                <path d='M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z'></path>
-              </svg>
+              <FaBars />
             </button>
           </div>
           <div className='links-container' style={{ height: '0px' }}>
